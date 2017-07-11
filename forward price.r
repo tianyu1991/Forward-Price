fb0da94230f37e336f8b58cd83ff10a1838b5c6d
tianyu liu <hulan1991@gmail.com>
2017-07-11 16:20:02 -0400

    Update forward price.r

--- forward price.r
+++ forward price.r
@@ -14,7 +14,7 @@ FP <- function(PY,i,time=1,C1=0,C1_time=0,C2=0,C2_time=0,c=0,y=0){
 	return(ForwardPrice)
 }
 ##FP(PY,i,time=1,C1=0,C1_time=0,C2=0,C2_time=0,c=0,y=0)
-##  Input variable:
+##  Input variables:
 ##  PY: Present value (require)
 ##  i: Continually compounded interest rate (require)
 ##  time: Forward contract time period (year), default:1
@@ -24,3 +24,24 @@ FP <- function(PY,i,time=1,C1=0,C1_time=0,C2=0,C2_time=0,c=0,y=0){
 ##  C1_time: Time of first coupon (year), default: 0
 ##  c: Continually compounded cost yield, default: 0
 ##  y: Continually compounded convenient yield, default: 0
+
+
+##Convert interest rate based on different compounding frequencies
+library(SciViews)
+ConY<-function(i,count=12,convert=0){
+	x<-1+i/count
+	x<-x^count
+	if(convert==0){
+		return(ln(x))}
+	else{
+		x<-x^(1/convert)-1
+		return(c*x)}
+}
+
+##Cony(i,count=12,convert=0)
+##Input Varibales:
+##i: Interest rate need to be converted
+##count: Compounding frequencies on a year
+##convert: Compounding frequencies on a year
+##         0 if need a continually compounded yield
+##         default: 0
